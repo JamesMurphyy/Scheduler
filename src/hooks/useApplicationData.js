8ec5090372ea61c7f6783b602481598f1e945fe3
James Murphy <jamesm8228@gmail.com>
2022-03-30 20:42:06 -0400

    Add changes to styling and code consistency

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
+
+//Hook made to clean up code in other file -- Application.js --
+
 export default function useApplicationData() {
 
-  ////////////////////
+  //Makes states for all of the properties that are required for Application.js file.
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -12,14 +15,14 @@ export default function useApplicationData() {
     interviewers: {}
   });
 
-
+  //Function from helpers folder that gets the certain appointments for the day that has been selected.
   let dailyAppointments = [];
-  dailyAppointments = getAppointmentsForDay(state, state.day)
+  dailyAppointments = getAppointmentsForDay(state, state.day);
 
-
+  //Sets the day as the day that has been selected.
   const setDay = day => setState({ ...state, day });
 
-
+  //useEffect used alongside a Promise.all feature that utilizes axios.get from the active database (api requests), and when completed, stores the information through states.
   useEffect(() => {
     Promise.all([
       axios.get(`/api/days`),
@@ -29,13 +32,14 @@ export default function useApplicationData() {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
   }, []);
-  //////////////////
+
+  //Update spots function that is triggered whenever a user adds or deletes an appointment. (Updates the spots under each day without having to reload the webpage/crash/reset the page).
   const updateSpots = function (state, id) {
     //Finding the day that is current and the one that will be updated
     const currentDay = state.days.find((day) => day.appointments.includes(id));
     //Finding appointments that are null (falsy)
-    const nullAppointments = currentDay.appointments.filter(id => !state.appointments[id].interview)
-    const spots = nullAppointments.length
+    const nullAppointments = currentDay.appointments.filter(id => !state.appointments[id].interview);
+    const spots = nullAppointments.length;
     //Making a newDay with the newly updated spots value
     const newDay = { ...currentDay, spots };
     //Checking to impliment the newDay value, just making a new array (map function) and updating the day if the name is correct (updating the spots value)
@@ -45,8 +49,8 @@ export default function useApplicationData() {
     return newDays;
   };
 
-
-  /////////////////////
+  //bookInterview function used to set the state of the appointments and interviews, that sends a put request through axios updating the information and also triggering an update to
+  //the updateSpots function mentioned above. 
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -62,10 +66,11 @@ export default function useApplicationData() {
     });
     return axios.put(`/api/appointments/${id}`, { interview }).then(response => {
       updateSpots(updateState, id);
-    })
-  }
+    });
+  };
 
-
+  //cancelInterview function used to set the state of the appointments and interviews, that sends a put request through axios updating the information and also triggering an update to
+  //the updateSpots function mentioned above (same as booking -- just instead of adding to the database, this function is removing from database in real time). 
   function cancelInterview(id) {
     const appointment = {
       ...state.appointments[id],
@@ -82,9 +87,7 @@ export default function useApplicationData() {
     return axios.delete(`/api/appointments/${id}`).then(response => {
       updateSpots(updateState, id);
     });
-  }
-  /////////////////
-
+  };
 
   return {
     state,
@@ -94,5 +97,5 @@ export default function useApplicationData() {
     bookInterview,
     cancelInterview,
     dailyAppointments
-  }
-}
+  };
+};
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,36 +1,37 @@
 import { useState } from 'react';
 
+//Defining a Hook function used to clean the code within the -- Index.js -- function/component
+
 export default function useVisualMode(initial) {
+
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-
+  //Function made that transitions the page to another display when called upon.
 
   function transition(newMode, replace = false) {
     if (replace) {
-
-      const newHistory = [...history]
+      const newHistory = [...history];
       newHistory.pop();
-      setHistory(prev => [...newHistory, newMode])
-      setMode(newMode)
+      setHistory(prev => [...newHistory, newMode]);
+      setMode(newMode);
       return;
 
     } else {
-
-      setMode(newMode)
-      setHistory(prev => [...prev, newMode])
+      setMode(newMode);
+      setHistory(prev => [...prev, newMode]);
       return;
-      
     }
   }
 
+  //Function made that moves the user back to the spot they were previously viewing -- making the user experience seamless and appearing to be effortless.
+
   function back() {
     if (history.length > 1) {
-      setMode(history[history.length - 2])
+      setMode(history[history.length - 2]);
       setHistory(prev => prev.slice(0, prev.length - 1));
       return;
     }
   }
-
   return { mode, transition, back };
 }
